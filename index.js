fe0f759a054f42839a1b14c07b27de35057d7c5e
helenlittle <42922152+helenlittle@users.noreply.github.com>
2019-03-04 16:10:38 -0600

    Create index.js
    Class change for scroll to top button based on window scroll

---
+++ index.js
@@ -0,0 +1,11 @@
+window.onscroll = function() {scroll_button()};
+
+function scroll_button() {
+  let breakpoint = 300;
+  if (document.body.scrollTop > breakpoint || document.documentElement.scrollTop > breakpoint) {
+    document.getElementById("scroll-button").className = "visible";
+  }  else if (document.body.scrollTop < breakpoint || document.documentElement.scrollTop < breakpoint) {
+    document.getElementById("scroll-button").className = "not-visible";
+  }
+  
+}
