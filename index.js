56f625ec9f800a1b133127f275c9372a5fc704b0
helenlittle <42922152+helenlittle@users.noreply.github.com>
2019-03-04 16:12:46 -0600

    Update index.js
    increased breakpoint to further down the page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onscroll = function() {scroll_button()};
 
 function scroll_button() {
-  let breakpoint = 300;
+  let breakpoint = 1000;
   if (document.body.scrollTop > breakpoint || document.documentElement.scrollTop > breakpoint) {
     document.getElementById("scroll-button").className = "visible";
   }  else if (document.body.scrollTop < breakpoint || document.documentElement.scrollTop < breakpoint) {
